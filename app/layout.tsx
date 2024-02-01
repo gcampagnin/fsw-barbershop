3ea9db62dda45640e285f41ee43fd7c3057c0dda
Gustavo Campagnin <gcampagnin@gmail.com>
2024-02-01 15:11:25 -0300

    feat: add service booking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
 import AuthProvider from "./_providers/auth";
+import { Toaster } from "./_components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>
-		    <AuthProvider>
-					{children}
-        	<Footer />
-				</AuthProvider>
-	    </body>
+        <AuthProvider>
+          {children}
+          <Toaster />
+          <Footer />
+        </AuthProvider>
+      </body>
     </html>
   );
 }
